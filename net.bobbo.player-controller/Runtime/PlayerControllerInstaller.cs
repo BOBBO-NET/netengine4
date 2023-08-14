037c0fb8f144a0a56252668e4d411376561b8c56
Iseeicy <iseeicyhawkston@gmail.com>
2023-08-13 21:11:57 -0500

    Player controller installer resolves

--- net.bobbo.player-controller/Runtime/PlayerControllerInstaller.cs
+++ net.bobbo.player-controller/Runtime/PlayerControllerInstaller.cs
@@ -8,20 +8,22 @@ namespace BobboNet.Player
     [CreateAssetMenu(fileName = "PlayerControllerInstaller", menuName = "Installers/PlayerControllerInstaller")]
     public class PlayerControllerInstaller : ScriptableObjectInstaller<PlayerControllerInstaller>
     {
-        public List<ScriptableObjectInstaller> behaviourInstallers = new List<ScriptableObjectInstaller>();
+        [SerializeField] List<ScriptableObjectInstallerBase> _behaviourInstallers = new List<ScriptableObjectInstallerBase>();
+        public IReadOnlyList<ScriptableObjectInstallerBase> BehaviourInstallers => _behaviourInstallers;
 
         public override void InstallBindings()
         {
-            Container.BindFactory<PlayerController, PlayerController.Factory>().FromSubContainerResolve().ByNewGameObjectMethod(InstallPlayerControllerFacade);
+            Container.BindFactory<PlayerController, PlayerController.Factory>().FromSubContainerResolve().ByNewGameObjectMethod(InstallPlayerControllerFacade).WithGameObjectName("PlayerController");
         }
 
         void InstallPlayerControllerFacade(DiContainer subContainer)
         {
-            subContainer.Bind<PlayerController>().AsSingle();
+            subContainer.Bind<PlayerController>().FromNewComponentOnRoot().AsSingle();
 
-            foreach (var behaviourInstaller in behaviourInstallers)
+            foreach (var behaviourInstaller in _behaviourInstallers)
             {
-                // behaviourInstaller.Install(subContainer);
+                subContainer.Inject(behaviourInstaller);
+                behaviourInstaller.InstallBindings();
             }
         }
     }
